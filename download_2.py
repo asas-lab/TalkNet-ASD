ffe2a2988a81e20610ab2351df6fe9891c7658c3
Raghad <raghadkhaled_24@hotmail.com>
2023-08-05 14:46:49 +0300

    added another download file

---
+++ download_2.py
@@ -0,0 +1,82 @@
+import os, uuid, json, argparse, warnings
+
+import pandas as pd
+
+from pytube import YouTube, Search, Playlist
+from pytube.exceptions import VideoUnavailable
+
+
+
+warnings.filterwarnings("ignore")
+
+parser = argparse.ArgumentParser(description = "Download YouTube Videos")
+
+parser.add_argument('--videoList',             type=str,  help='A CVS file containing a list of YouTube video URLs')
+parser.add_argument('--videoFolder',           type=str, default="videos",  help='Path for inputs, tmps and outputs')
+
+args = parser.parse_args()
+
+
+def download(cvs_urls: str, out_dir: str):
+
+    """
+    Input:
+        cvs_urls: A list of urls in the first col in CVS
+        out_dir: A folder path to save the downloaded videos
+    Return:
+        download all videos in each podcast playlist
+        json file conatining meta info about each video
+
+    """
+    podcasts = pd.read_csv(cvs_urls)
+    podcasts_urls = podcasts.iloc[:, 1]
+
+    # create directory folder for each podcast
+    for i, url in enumerate(podcasts_urls):
+        downloaded_podcast_path = os.path.join(out_dir, "podcast_" + str(i))
+        if not os.path.exists(downloaded_podcast_path):
+            os.makedirs(downloaded_podcast_path)
+
+        # find all YouTube videos in podcast playlist
+        yt = Playlist(url)
+
+        meta_info = {}
+        id = str(uuid.uuid4())
+
+        for video_id, video_url in enumerate(yt):
+            filename = str(video_id) + '.mp4'
+            try:
+
+                video = YouTube(video_url, use_oauth=True,
+                                allow_oauth_cache=True)
+                video.streams. \
+                    filter(type='video', progressive=True, file_extension='mp4'). \
+                    order_by('resolution'). \
+                    desc(). \
+                    first(). \
+                    download(output_path=downloaded_podcast_path, filename=filename)
+
+                meta_info[id] = {
+                    "video_title": video.title,
+                    "yt_url": video_url,
+                    "video_file_name": filename,
+                    "yt_channel_url": video.channel_url,
+                    "length": video.length
+                }
+
+            except:
+                print("Download Error!")
+
+        #   with open("talking_head_info.json", "w") as outfile:
+        #     json.dump(meta_info, outfile)
+
+
+def main():
+   
+    args.pyaviPath = os.path.join(args.videoFolder)
+    os.makedirs(args.pyaviPath, exist_ok = True) # The path for the input video, input audio, output video
+    download(args.videoList, args.pyaviPath)
+
+
+if __name__ == '__main__':
+    main()
